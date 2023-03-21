94ef4dff83f53c8a76734d82aa1c574ff848084a
zFlxw <lxcalflxw@gmail.com>
2023-03-21 22:26:51 +0100

    feat: navbar start

---
+++ src/lib/NavbarComponent.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { Menu } from 'lucide-svelte';
+</script>
+
+<nav class="flex items-center justify-between px-4 py-2 border-b border-b-neutral-100 w-full">
+    <img src="/img/avatars/blue-avatar-crop.png" alt="wolf-avatar" class="w-[40px]" />
+    <p class="text-2xl">Homepage</p>
+    <Menu  size={36} strokeWidth={1} />
+</nav>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,32 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'ibm-plex-mono': ['IBM Plex Mono', 'monospace']
+      }
+    },
+    colors: {
+      neutral: {
+        50: '#F5F5F5',
+        100: '#DDDDDD',
+        200: '#C4C4C4',
+        300: '#ACACAC',
+        400: '#939393',
+        500: '7B7B7B', 
+        600: '#626262',
+        700: '#494949',
+        800: '#313131',
+        900: '#181818'
+      },
+      primary: {
+        blue: '#0075F2',
+        red: {
+          400: '#CF5653',
+          500: '#E65F5C',
+        }
+      }
+    }
   },
   plugins: [],
 }

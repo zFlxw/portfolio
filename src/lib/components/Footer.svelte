9de75bedafb39ee2acfb6a663c337452367756b2
zFlxw <lxcalflxw@gmail.com>
2023-03-24 22:10:07 +0100

    feat: homepage design

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Wrench } from "lucide-svelte";
+</script>
+
+<div class="text-neutral-400">
+  <Wrench size={32} strokeWidth={1.5} class="absolute bottom-2 left-5 hoverable"/>
+  <a href="/legal" class="absolute bottom-2 right-5 hover:underline">Legal Notice</a>
+</div>

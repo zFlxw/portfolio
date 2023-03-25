8123b1d8588ec3fbea205add6d509ef9a5dc1a01
zFlxw <lxcalflxw@gmail.com>
2023-03-25 19:55:53 +0100

    fix: mobile things

--- src/lib/components/FooterComponent.svelte
+++ src/lib/components/FooterComponent.svelte
@@ -2,7 +2,7 @@
   import { Wrench } from "lucide-svelte";
 </script>
 
-<div class="text-neutral-400 flex justify-between items-center mt-8 md:mt-0 mb-2 mx-8">
-  <Wrench size={32} strokeWidth={1.5} class="bhoverable"/>
+<div class="text-neutral-400 flex justify-center xl:justify-between items-center mt-8 lg:mt-0 mb-2 mx-8">
+  <Wrench size={32} strokeWidth={1.5} class="hidden xl:block hoverable"/>
   <a href="/legal" class="hover:underline">Legal Notice</a>
 </div>
